c443cb9d80147b5dcb8ef97899c1888d00ccf98f
dubeyaman <45045837+dubeyaman@users.noreply.github.com>
2022-11-27 15:45:25 +0530

    Delete main.js
    k

--- main.js
+++
@@ -1,9 +0,0 @@
-//const votes=document.getElementsByTagName("vote")
-const increment=document.getElementsByTagName('increment')
-console.log('aman')
-let count=0;
-
-// votes.addEventListener('click',()=>{
-//     count++;
-//     increment.innerHTML=count
-// })
