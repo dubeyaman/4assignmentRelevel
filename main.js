769019dd00386879fe634f92fefe2c99cab53e49
dubeyaman <45045837+dubeyaman@users.noreply.github.com>
2022-11-27 15:44:09 +0530

    Add files via upload
    working fine

---
+++ main.js
@@ -0,0 +1,9 @@
+//const votes=document.getElementsByTagName("vote")
+const increment=document.getElementsByTagName('increment')
+console.log('aman')
+let count=0;
+
+// votes.addEventListener('click',()=>{
+//     count++;
+//     increment.innerHTML=count
+// })
